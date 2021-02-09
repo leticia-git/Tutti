793c3e3c96365affe690f4bd113cd0eef347b0b7
leticia-git <leticia.araujo.oliveira03@outlook.com>
2021-02-08 21:27:00 -0300

    novo commit

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use('/login', loginRouter);
 app.use('/recipes', recipesRouter);
 app.use('/us', usRouter);
 app.use('/register', registerRouter);
+app.use(express.static('public'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tutti' });
 });
 
+
+
+
 module.exports = router;
+
+
