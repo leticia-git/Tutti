0135235c16c98b190d1588729a279a6c558dc09b
leticia-git <leticia.araujo.oliveira03@outlook.com>
2021-04-28 22:10:48 -0300

    Organizando objetos para banco

--- data/card.js
+++ data/card.js
@@ -11,6 +11,117 @@ let cards = [
     'texto': '3,99 kg',
     'botao': 'Comprar'
   },
+  {
+    'caminho_de_imagem': '/images/ameixa.png',
+    'titulo': 'Ameixa', 
+    'texto': '25,99 kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/amora.png',
+    'titulo': 'Amora', 
+    'texto': '34,99 Kg',
+    'botao': 'Comprar'
+  },
+
+  {
+    'caminho_de_imagem': '/images/banana1.png',
+    'titulo': 'Banana', 
+    'texto': '2,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/cereja.png',
+    'titulo': 'Cereja', 
+    'texto': '49,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/coco.png',
+    'titulo': 'Coco', 
+    'texto': '2,99 Un',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/goiaba1.png',
+    'titulo': 'Goiaba', 
+    'texto': '4,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/kiwi.png',
+    'titulo': 'Kiwi', 
+    'texto': '23,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/limao.png',
+    'titulo': 'Limão', 
+    'texto': '2,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/maça1.png',
+    'titulo': 'Maçã', 
+    'texto': '2,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/maçaverde.png',
+    'titulo': 'Maçã Verde', 
+    'texto': '5,99 Kg',
+    'botao': 'Comprar'
+  },
+
+  {
+    'caminho_de_imagem': '/images/manga.png',
+    'titulo': 'Manga', 
+    'texto': '5,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/melancia.png',
+    'titulo': 'Melancia', 
+    'texto': '9,99 Un',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/morango.png',
+    'titulo': 'Morango', 
+    'texto': '5,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/orange.png',
+    'titulo': 'Laranja', 
+    'texto': '2,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/papaya.png',
+    'titulo': 'Mamão', 
+    'texto': '5,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/pêssego.png',
+    'titulo': 'Pêssego', 
+    'texto': '7,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/uva.png',
+    'titulo': 'Uva', 
+    'texto': '6,99 Kg',
+    'botao': 'Comprar'
+  },
+  {
+    'caminho_de_imagem': '/images/pera.png',
+    'titulo': 'Perâ', 
+    'texto': '6,99 Kg',
+    'botao': 'Comprar'
+  },
+
 ];
 
 module.exports = cards;
