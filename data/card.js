79d47726f2d36efa869f84b0c2c72c6e0ca62a61
leticia-git <leticia.araujo.oliveira03@outlook.com>
2021-05-01 15:22:39 -0300

    modificando cards

--- data/card.js
+++ data/card.js
@@ -195,7 +195,366 @@ let cards = [
     'categoryId':1,
     'botao': 'Comprar'
   },
-
+  {
+    'id': 1,
+    'name': 'Abobora', 
+    'picture': '/images/abobora1.png',
+    'description':'',
+    'price': '5,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 2,
+    'name': 'Abobrinha', 
+    'picture': '/images/abobrinha.png',
+    'description':'',
+    'price': '5,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 3,
+    'name': 'Aspargos', 
+    'picture': '/images/aspargos.png',
+    'description':'',
+    'price': '53,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 4,
+    'name': 'Batata', 
+    'picture': '/images/batata.png',
+    'description':'',
+    'price': '3,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 5,
+    'name': 'Batata doce', 
+    'picture': '/images/batatadoce.png',
+    'description':'',
+    'price': '3,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 6,
+    'name': 'Beringela', 
+    'picture': '/images/beringela.png',
+    'description':'',
+    'price': '8,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 7,
+    'name': 'Beterraba', 
+    'picture': '/images/beterraba1.png',
+    'description':'',
+    'price': '5,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 8,
+    'name': 'Cenoura', 
+    'picture': '/images/cenoura.png',
+    'description':'',
+    'price': '4,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 9,
+    'name': 'Chuchu', 
+    'picture': '/images/chuchu.png',
+    'description':'',
+    'price': '2,99kg ',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 10,
+    'name': 'Inhame', 
+    'picture': '/images/inhame.png',
+    'description':'',
+    'price': '4,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 11,
+    'name': 'Mandioca', 
+    'picture': '/images/mandioca.png',
+    'description':'',
+    'price': '7,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 12,
+    'name': 'Mandioquinha', 
+    'picture': '/images/mandioquinha.png',
+    'description':'',
+    'price': '5,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 13,
+    'name': 'Nabo', 
+    'picture': '/images/nabo.png',
+    'description':'',
+    'price': '4,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 14,
+    'name': 'Palmito', 
+    'picture': '/images/palmito.png',
+    'description':'',
+    'price': '24,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 15,
+    'name': 'Pepino', 
+    'picture': '/images/pepino.png',
+    'description':'',
+    'price': '2,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 16,
+    'name': 'Pimentao', 
+    'picture': '/images/pimentao.png',
+    'description':'',
+    'price': '2,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 17,
+    'name': 'Pimentao Amarelo', 
+    'picture': '/images/pimentaoamarelo.png',
+    'description':'',
+    'price': '8,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 18,
+    'name': 'Quiabo', 
+    'picture': '/images/quiabo.png',
+    'description':'',
+    'price': '3,60 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 19,
+    'name': 'Rabanete', 
+    'picture': '/images/rabanete.png',
+    'description':'',
+    'price': '5,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 20,
+    'name': 'Tomate', 
+    'picture': '/images/tomate.png',
+    'description':'',
+    'price': '4,99 kg',
+    'categoryId':2,
+    'botao': 'Comprar'
+  },
+    {
+    'id': 1,
+    'name': 'Acélga', 
+    'picture': '/images/acelga1.png',
+    'description':'',
+    'price': '4,99 kg',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 2,
+    'name': 'Agião', 
+    'picture': '/images/agriao.png',
+    'description':'',
+    'price': '1,60 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 3,
+    'name': 'Alface Americana', 
+    'picture': '/images/alfaceamericana.png',
+    'description':'',
+    'price': '5,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 4,
+    'name': 'Alface Crespa', 
+    'picture': '/images/alfacecrespa.png',
+    'description':'',
+    'price': '1,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 5,
+    'name': 'Alho Poró', 
+    'picture': '/images/alhoporo.png',
+    'description':'',
+    'price': '4,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 6,
+    'name': 'Almeirão', 
+    'picture': '/images/almeirao.png',
+    'description':'',
+    'price': '2,49 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 7,
+    'name': 'Brocolis', 
+    'picture': '/images/brocolis1.png',
+    'description':'',
+    'price': '5,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 8,
+    'name': 'Cebola', 
+    'picture': '/images/cebola.png',
+    'description':'',
+    'price': '2,99 kg',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 9,
+    'name': 'Cebola Roxa', 
+    'picture': '/images/cebolaroxa.png',
+    'description':'',
+    'price': '7,99 kg',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 10,
+    'name': 'Chicoria', 
+    'picture': '/images/chicoria.png',
+    'description':'',
+    'price': '3,89 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 11,
+    'name': 'Coentro', 
+    'picture': '/images/coentro.png',
+    'description':'',
+    'price': '3,89 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 12,
+    'name': 'Couve-Flor', 
+    'picture': '/images/couveflor.png',
+    'description':'',
+    'price': '5,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 13,
+    'name': 'Couve-manteiga', 
+    'picture': '/images/couvemanteiga.png',
+    'description':'',
+    'price': '4,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 14,
+    'name': 'Espinafre', 
+    'picture': '/images/espinafre.png',
+    'description':'',
+    'price': '3,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 15,
+    'name': 'Hortela', 
+    'picture': '/images/hortela.png',
+    'description':'',
+    'price': '79,99 Kg',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 16,
+    'name': 'Repolho', 
+    'picture': '/images/repolho1.png',
+    'description':'',
+    'price': '2,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 17,
+    'name': 'Repolho Roxo', 
+    'picture': '/images/repolhoroxo.png',
+    'description':'',
+    'price': '3,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 18,
+    'name': 'Rucula ', 
+    'picture': '/images/rucula.png',
+    'description':'',
+    'price': '4,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 19,
+    'name': 'Salsão', 
+    'picture': '/images/salsao.png',
+    'description':'',
+    'price': '6,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
+  {
+    'id': 20,
+    'name': 'Cebolinha', 
+    'picture': '/images/cebolinha.png',
+    'description':'',
+    'price': '6,99 Un',
+    'categoryId':3,
+    'botao': 'Comprar'
+  },
 ];
 
 module.exports = cards;
--- routes/mercadoB.js
+++ routes/mercadoB.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const cards = require('../data/card')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mercadoB');
+  res.render('mercadoB', {cards: cards, user:req.session.user});
 }); 
 
 module.exports = router;
