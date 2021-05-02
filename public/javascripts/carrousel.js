f32133aeec6bb9b08e1cf1e04aa80113a0f59d87
leticia-git <leticia.araujo.oliveira03@outlook.com>
2021-05-02 14:14:34 -0300

    atulizando detalhes do front

--- public/javascripts/carrousel.js
+++ public/javascripts/carrousel.js
@@ -36,3 +36,14 @@ $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
 
   return false;
 });
+
+
+function process(quant){
+  var value = parseInt(document.getElementById("quant").value);
+  value+=quant;
+  if(value < 1){
+    document.getElementById("quant").value = 1;
+  }else{
+  document.getElementById("quant").value = value;
+  }
+}
