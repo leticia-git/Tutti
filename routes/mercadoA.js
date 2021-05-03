84d9db0bde8598a14ec860962ae81b47d1e925a5
leticia-git <leticia.araujo.oliveira03@outlook.com>
2021-05-02 23:20:38 -0300

    Atualizando cart

--- routes/mercadoA.js
+++ routes/mercadoA.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const cards = require('../data/card')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mercadoA', {cards: cards, user:req.session.user});
+  res.render('mercadoA');
 }); 
 
 module.exports = router;
